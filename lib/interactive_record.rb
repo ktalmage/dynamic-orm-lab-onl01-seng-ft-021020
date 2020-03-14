a5a70a0c01bfeb06744d5d77a4c6241002016734
Kyle Talmage <ktalmage85@gmail.com>
2020-03-14 14:31:07 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -1,6 +1,6 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
-
+require 'pry'
 class InteractiveRecord
    def self.table_name
     self.to_s.downcase.pluralize
@@ -50,6 +50,7 @@ class InteractiveRecord
   def self.find_by_name(name)
     sql = "SELECT * FROM #{self.table_name} WHERE name = ?"
     DB[:conn].execute(sql, name)
+    binding.pry
   end
 
   def self.find_by(id)
