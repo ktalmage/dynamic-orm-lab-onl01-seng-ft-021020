a811004dc5c223dfb076091fdd2b3e91c793b9f8
Kyle Talmage <ktalmage85@gmail.com>
2020-03-14 14:53:43 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -58,9 +58,6 @@ class InteractiveRecord
     attribute_value = attribute.values.first
     sql = "SELECT * FROM #{self.table_name}
      WHERE #{attribute_key} = #{attribute_value}"
-     DB[:conn].execute(sql,grade)
-     
-     SQL
      DB[:conn].execute(sql)
   end
 end
