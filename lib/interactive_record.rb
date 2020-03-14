7605cc1b73aa035a8886db1aad38976b40e5b011
Kyle Talmage <ktalmage85@gmail.com>
2020-03-14 14:36:03 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -50,7 +50,7 @@ class InteractiveRecord
   def self.find_by_name(name)
     sql = "SELECT * FROM #{self.table_name} WHERE name = ?"
     DB[:conn].execute(sql, name)
-    binding.pry
+  
   end
 
   def self.find_by(grade)
