11d50ef5c1d2402466c9d2e12616bcc2c36ceb69
Kyle Talmage <ktalmage85@gmail.com>
2020-03-14 14:56:32 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -53,12 +53,11 @@ class InteractiveRecord
   
   end
 
-  def self.find_by(attribute)
-    attribute_key = attribute.keys.join()
-    attribute_value = attribute.values.first
-    sql = "SELECT * FROM #{self.table_name}
-     WHERE #{attribute_key} = #{attribute_value} LIMIT 1"
-     row = DB[:conn].execute(sql)
+  def self.find_by(attribute_hash)
+    value = attribute_hash.values.first
+    formatted_value = value.class == Fixnum ? value : "'#{value}'"
+    sql = "SELECT * FROM #{self.table_name} WHERE #{attribute_hash.keys.first} = #{formatted_value}"
+    DB[:conn].execute(sql)
   end
 end
 
